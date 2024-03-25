3f650f16580cdefdc208f8e9673ef6518e88525c
iRatkin <ADH.2234@gmail.com>
2024-03-26 01:48:06 +0300

    New components

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router';
 import './App.css';
 import Courses from './components/Courses';
 import CreateCourse from './components/CreateCourse';
+import Header from './components/Header';
+import EditCourse from './components/EditCourse';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <CreateCourse />
-        <Courses />
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Courses} />
+          <Route exact path="/create" component={CreateCourse} />
+          <Route exact path="/course/:id/edit" component={EditCourse} />
+        </Switch>
       </div>
     );
   }
--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -1,20 +1,8 @@
 import React from 'react';
 import { Query } from 'react-apollo';
 import { gql } from 'apollo-boost';
+import { Link } from 'react-router-dom';
 
-const renderCourses = ({ data, error, loading }) => {
-  if (loading) return <p>...Loading</p>;
-  if (error) return <p>Error</p>;
-  return data.courseFeed.map(({ id, description, name, isPublished }) => {
-    return (
-      <div key={id}>
-        <p>Name: {name} </p>
-        <p>Description: {description} </p>
-        <hr />
-      </div>
-    );
-  });
-};
 const Courses = () => {
   return (
     <div>
@@ -36,9 +24,14 @@ const Courses = () => {
           return data.courseFeed.map(
             ({ id, description, name, isPublished }) => {
               return (
-                <div key={id}>
-                  <p>Name: {name} </p>
-                  <p>Description: {description} </p>
+                <div key={id} className="card container">
+                  <div className="card-body">
+                    <h5 className="card-title">{name}</h5>
+                    <p className="card-text"> {description} </p>
+                    <Link to={`course/${id}/edit`} className="btn btn-primary">
+                      Edit
+                    </Link>
+                  </div>
                 </div>
               );
             }
