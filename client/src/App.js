afcdd6014d0a68b33398b829b9eb6b1db521acae
iRatkin <ADH.2234@gmail.com>
2024-03-25 23:12:58 +0300

    Frontend added

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Courses from './Courses';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Courses />
+      </div>
+    );
+  }
+}
+
+export default App;
